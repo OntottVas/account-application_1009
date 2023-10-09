d77035761a9772b6ea01027eeb8709ad6068fcd6
Geree <smithzero94@gmail.com>
2023-10-09 17:59:50 +0200

    spring boot project set up

---
+++ src/main/java/hu/progmatic/accountapplication_1009/AccountApplication1009Application.java
@@ -0,0 +1,13 @@
+package hu.progmatic.accountapplication_1009;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountApplication1009Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication1009Application.class, args);
+    }
+
+}
---
+++ src/test/java/hu/progmatic/accountapplication_1009/AccountApplication1009ApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.progmatic.accountapplication_1009;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApplication1009ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
